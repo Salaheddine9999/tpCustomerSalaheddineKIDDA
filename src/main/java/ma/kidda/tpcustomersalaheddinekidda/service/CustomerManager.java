612c30829bef02ca69b3bc70d3c889ad324e4968
Salaheddine KIDDA <salaheddinekidda99@gmail.com>
2023-10-24 00:38:46 +0200

    Ajout CustomerManager

---
+++ src/main/java/ma/kidda/tpcustomersalaheddinekidda/service/CustomerManager.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.kidda.tpcustomersalaheddinekidda.service;
+
+import ma.kidda.tpcustomersalaheddinekidda.entity.Customer;
+
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+/**
+ *
+ * Gère la persistance des Customers
+ */
+
+@RequestScoped
+public class CustomerManager {
+    
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+    
+    public List<Customer> getAllCustomers() {  
+        Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList(); 
+    }  
+
+    @Transactional
+    public Customer update(Customer customer) {
+      return null;  
+    }   
+    
+     public void persist(Customer customer) {
+       em.persist(customer);
+    }
+}
